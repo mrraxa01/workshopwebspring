4f7c2a6e01347f0dc63b303a307e47b603ba680b
mrraxa01 <mrraxa01@gmail.com>
2020-10-15 22:22:26 -0300

    H2 database, test profile, JPA

--- src/main/java/com/mrraxa/course/entities/User.java
+++ src/main/java/com/mrraxa/course/entities/User.java
@@ -2,10 +2,19 @@ package com.mrraxa.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-
+	//defina como chave primária
+	@Id
+	//auto incremento do banco de dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
