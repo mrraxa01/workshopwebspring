82b6fe916c1fa0ef6b05e5b56e0ec097defc4b95
mrraxa01 <mrraxa01@gmail.com>
2020-10-19 22:53:32 -0300

    Exception handling - delete

--- src/main/java/com/mrraxa/course/services/UserService.java
+++ src/main/java/com/mrraxa/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mrraxa.course.entities.User;
 import com.mrraxa.course.repositories.UserRepository;
+import com.mrraxa.course.services.exceptions.DatabaseException;
 import com.mrraxa.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,7 +36,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+		
 	}
 	
 	public User update(Long id, User obj) {
